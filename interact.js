9d73bdc4bdaf73c6e1cb36510a62f4b281c2e02d
DocQuantum <shchurgood@gmail.com>
2019-01-20 03:35:15 -0600

    fixed auth issue, fixed text area null issue, minimized stitch.js

--- interact.js
+++ interact.js
@@ -1,20 +1,26 @@
 function sendReport() {
-  console.log("radio" + document.querySelector('input[name="emojiRadio"]:checked').value);
-  console.log("whatWasWrong" + document.getElementById('whatWasWrong').select());
-  console.log("howToFix" + document.getElementById('howToFix').select());
+  try {
+    console.log("radio: " + document.querySelector('input[name="emojiRadio"]:checked').value);
+  } catch (e) {
+    console.log("radio not selected");
+  }
+  console.log("whatWasWrong: " + document.getElementById('whatWasWrong').value);
+  console.log("howToFix: " + document.getElementById('howToFix').value);
   const client = stitch.Stitch.initializeDefaultAppClient('canaryapi-uwhnp');
   const mongodb = client.getServiceClient(stitch.RemoteMongoClient.factory, 'mongodb-atlas').db('canary');
   const chromeVersion = /Chrome\/([0-9.]+)/.exec(navigator.userAgent)[1];
+  client.auth.loginWithCredential(new stitch.AnonymousCredential()).then(user =>
   mongodb.collection("reports")
     .insertOne({
       imageURL: "test.com/test.jpg",
       emoji: document.querySelector('input[name="emojiRadio"]:checked').value,
-      whatWasWrong: document.getElementById('whatWasWrong').select(),
-      howToFix: document.getElementById('howToFix').select(),
+      whatWasWrong: document.getElementById('whatWasWrong').value,
+      howToFix: document.getElementById('howToFix').value,
       pageURL: "test.com",
       browserVer: chromeVersion,
       osVer: window.navigator.userAgent
-    });
+    })
+  )
 };
 
 document.getElementById("submitButton").addEventListener("click", sendReport);
