86e23cec2b7df45f1461849a854b72ac98e1429f
forbesjon2 <forbesjon2@gmail.com>
2019-01-19 21:21:39 -0600

    addes some tings

--- capture.js
+++ capture.js
@@ -1,3 +1,10 @@
 chrome.tabs.captureVisibleTab(function(screenshotUrl) {
   document.getElementById('issue').src = screenshotUrl;
+  // var height = window.innerHeight - 30;
+  // var width = window.innerWidth - 30;
+  var image = document.getElementById('issue');
+  console.log(image.width);
+  console.log("aisidnf");
+  image.setAttribute("width",  500);
+  image.setAttribute("height", 500);
 });
---
+++ interact.js
@@ -0,0 +1,5 @@
+
+var submitButton = document.getElementById("submitButton");
+submitButton.onclick = function(){
+    console.log("ass");
+}
