d107f9f1b70bbce8f65d0760af519f653ec2dbcf
DocQuantum <shchurgood@gmail.com>
2019-01-20 06:05:28 -0600

    Improved input validation and error messages, added on success event.

--- interact.js
+++ interact.js
@@ -1,4 +1,5 @@
 function sendReport() {
+  document.getElementById('errorText').innerHTML = "";
   try {
     console.log("radio: " + document.querySelector('input[name="emojiRadio"]:checked').value);
   } catch (e) {
@@ -6,11 +7,11 @@ function sendReport() {
     document.getElementById('errorText').innerHTML = "Please select an emoji"
     return;
   }
-  if(document.getElementById('whatWasWrong').value == undefined){
+  if(document.getElementById('whatWasWrong').value == ""){
     console.log("No issue text");
     document.getElementById('errorText').innerHTML = "Please tell us what was wrong"
     return;
-  } else if(document.getElementById('howToFix').value == undefined){
+  } else if(document.getElementById('howToFix').value == ""){
     console.log("No impovement text");
     document.getElementById('errorText').innerHTML = "Please suggest us an improvement"
     return;
@@ -18,6 +19,7 @@ function sendReport() {
   // Testing
   console.log("whatWasWrong: " + document.getElementById('whatWasWrong').value);
   console.log("howToFix: " + document.getElementById('howToFix').value);
+  console.log("Date: " + Date())
   // get client for api calls to mongodb
   const client = stitch.Stitch.initializeDefaultAppClient('canaryapi-uwhnp');
   // select database
@@ -25,20 +27,24 @@ function sendReport() {
   // get chrome version
   const chromeVersion = /Chrome\/([0-9.]+)/.exec(navigator.userAgent)[1];
   // login as anon and post data to server
-  client.auth.loginWithCredential(new stitch.AnonymousCredential()).then(user =>
-    mongodb.collection("reports")
-    .insertOne({
-      imageURL: "test.com/test.jpg",
-      emoji: document.querySelector('input[name="emojiRadio"]:checked').value,
-      whatWasWrong: document.getElementById('whatWasWrong').value,
-      howToFix: document.getElementById('howToFix').value,
-      pageURL: "test.com",
-      browserVer: chromeVersion,
-      osVer: window.navigator.userAgent,
-      date: date();
-    })
+  client.auth.loginWithCredential(new stitch.AnonymousCredential()).then(user => mongodb.collection("reports")
+  .insertOne({
+    imageURL: "test.com/test.jpg",
+    emoji: Number(document.querySelector('input[name="emojiRadio"]:checked').value),
+    whatHappened: document.getElementById('whatWasWrong').value,
+    howToFix: document.getElementById('howToFix').value,
+    pageURL: "test.com",
+    browserVer: chromeVersion,
+    osVer: window.navigator.userAgent
+  })
   );
-  document.getElementById("submitButton").disabled = true;
+  thankUserForReport();
 };
 
 document.getElementById("submitButton").addEventListener("click", sendReport);
+
+function thankUserForReport(){
+  document.getElementById("submitButton").disabled = true;
+  document.getElementById("submitButton").innerHTML = "Thank you!";
+  document.getElementById('errorText').innerHTML = "";
+}
