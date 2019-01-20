0d527e2948de6dc5ed35a8034610ce1442faed1a
DocQuantum <shchurgood@gmail.com>
2019-01-20 05:17:25 -0600

    ui improvements including error checking and error text inplementation

--- interact.js
+++ interact.js
@@ -2,15 +2,31 @@ function sendReport() {
   try {
     console.log("radio: " + document.querySelector('input[name="emojiRadio"]:checked').value);
   } catch (e) {
-    console.log("radio not selected");
+    console.log("Emoji not selected");
+    document.getElementById('errorText').innerHTML = "Please select an emoji"
+    return;
   }
+  if(document.getElementById('whatWasWrong').value == undefined){
+    console.log("No issue text");
+    document.getElementById('errorText').innerHTML = "Please tell us what was wrong"
+    return;
+  } else if(document.getElementById('howToFix').value == undefined){
+    console.log("No impovement text");
+    document.getElementById('errorText').innerHTML = "Please suggest us an improvement"
+    return;
+  }
+  // Testing
   console.log("whatWasWrong: " + document.getElementById('whatWasWrong').value);
   console.log("howToFix: " + document.getElementById('howToFix').value);
+  // get client for api calls to mongodb
   const client = stitch.Stitch.initializeDefaultAppClient('canaryapi-uwhnp');
+  // select database
   const mongodb = client.getServiceClient(stitch.RemoteMongoClient.factory, 'mongodb-atlas').db('canary');
+  // get chrome version
   const chromeVersion = /Chrome\/([0-9.]+)/.exec(navigator.userAgent)[1];
+  // login as anon and post data to server
   client.auth.loginWithCredential(new stitch.AnonymousCredential()).then(user =>
-  mongodb.collection("reports")
+    mongodb.collection("reports")
     .insertOne({
       imageURL: "test.com/test.jpg",
       emoji: document.querySelector('input[name="emojiRadio"]:checked').value,
@@ -18,9 +34,11 @@ function sendReport() {
       howToFix: document.getElementById('howToFix').value,
       pageURL: "test.com",
       browserVer: chromeVersion,
-      osVer: window.navigator.userAgent
+      osVer: window.navigator.userAgent,
+      date: date();
     })
-  )
+  );
+  document.getElementById("submitButton").disabled = true;
 };
 
 document.getElementById("submitButton").addEventListener("click", sendReport);
